4f8937056e30a9f2b7834ba2a2637a04c83687da
Major <james.major@cgi.com>
2020-01-13 15:46:22 -0500

    Update code comments

--- Light.py
+++ Light.py
@@ -2,22 +2,23 @@ import RPi.GPIO as GPIO
 import time
 
 class Light():
-    
+
     # Initialise wheels
     def __init__(self, logger, pin):
+        self.logger.info("[ Light ]\t Initialisation..")
         self.logger = logger
         self.pin = pin
-        
+
         # Setting up GPIO pins
         self.logger.info("[ Light ]\t Setting up GPIO pin")
         GPIO.setup(self.pin, GPIO.OUT)
-    
+
     def turnOn(self):
         GPIO.output(self.pin, True)
-    
+
     def turnOff(self):
         GPIO.output(self.pin, False)
-        
+
     def flash(self, value):
         self.logger.info("[ Light ]\t Flashing..")
         for x in range(value):
@@ -25,4 +26,3 @@ class Light():
             time.sleep(0.5)
             self.turnOff()
             time.sleep(0.5)
-        
--- Wheels.py
+++ Wheels.py
@@ -1,50 +1,55 @@
 import RPi.GPIO as GPIO
 
 class Wheels():
-    
+
     # Initialise wheels
     def __init__(self, logger, pin_a1, pin_a2, pin_b1, pin_b2):
         self.logger = logger
-        self.logger.info("[ Wheels ]\t  Initialisation..")
-        
-        # Assigning pins
+        self.logger.info("[ Wheels ]\t Initialisation..")
+
+        # Assigning pins motor A
         self.pin_motor_a1 = pin_a1
         self.pin_motor_a2 = pin_a2
+
+        # Assigning pins motor B
         self.pin_motor_b1 = pin_b1
         self.pin_motor_b2 = pin_b2
-        
+
         # Setting up GPIO pins
         self.logger.info("[ Wheels ]\t Setting up GPIO pins")
         GPIO.setup(self.pin_motor_a1, GPIO.OUT)
         GPIO.setup(self.pin_motor_a2, GPIO.OUT)
         GPIO.setup(self.pin_motor_b1, GPIO.OUT)
         GPIO.setup(self.pin_motor_b2, GPIO.OUT)
-    
+
+    # Set motor A GPIOs values
     def setMotorA(self, pin_1, pin_2):
         GPIO.output(self.pin_motor_a1, pin_1)
         GPIO.output(self.pin_motor_a2, pin_2)
-        
+
+    # Set motor A GPIOs values
     def setMotorB(self, pin_1, pin_2):
         GPIO.output(self.pin_motor_b1, pin_1)
         GPIO.output(self.pin_motor_b2, pin_2)
-        
-    # Directions
+
+    # Directionals methods
     def goFoward(self):
         self.setMotorA(False, True)
         self.setMotorB(False, True)
-    
+
     def goBackward(self):
         self.setMotorA(True, False)
         self.setMotorB(True, False)
-        
+
     def turnRight(self):
         self.setMotorA(True, False)
         self.setMotorB(False, True)
-        
+
     def turnLeft(self):
         self.setMotorA(False, True)
         self.setMotorB(True, False)
-        
+
+    # All stop
     def stop(self):
         self.setMotorA(False, False)
         self.setMotorB(False, False)
